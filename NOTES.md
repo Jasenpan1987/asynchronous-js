2d261160a919c69ee02bcc61907633c94ba6c001
Jasenpan1987 <jasenpan@gmail.com>
2018-08-26 00:12:16 +1000

    thunk

--- NOTES.md
+++ NOTES.md
@@ -99,3 +99,106 @@ It still has the "trust" issue, for example what if we call the callback with th
 ### So in general there is no fix for these callback issues.
 
 ## 2. Thunks
+
+```js
+// synchronous simple thunk
+function add(x, y) {
+  return x + y;
+}
+
+var thunk = function() {
+  return add(10, 5);
+};
+
+thunk(); // 25
+```
+
+Thunk works like a wrapper, wrap around a function with given state (in this example, 10 and 5), and we can pass around the wrapper and give it to other functions. Once the thunk gets called, it will always used the same state that wrapped inside the function.
+
+```js
+// asynchronous thunk
+function addAsync(x, y, cb) {
+  setTimeout(function() {
+    cb(x + y);
+  }, 1000);
+}
+
+var thunk = function(cb) {
+  addAsync(4, 6, cb);
+};
+
+thunk(function(total) {
+  console.log("Total: " + total); // 10
+});
+```
+
+**Time is the most complex factor of state in our program**
+
+Compare with normal callback
+
+```js
+function addAsync(x, y) {
+  setTimeout(function() {
+    var total = x + y;
+    console.log("Total: " + total);
+  }, 1000);
+}
+```
+
+The good thing of thunk is it eliminate the impact of time. Here, in the normal callback, if there is a complex logic, it has be inside the callback. And for thunk, we can just define the thunk, and call it with a callback.
+
+### 2.1 Generalized thunk
+
+```js
+function makeThunk(fn) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  return function(cb) {
+    args.push(cb);
+    fn.apply(null, args);
+  };
+}
+```
+
+Let refactor our previous usecase of thunk
+
+```js
+function addAsync(x, y, cb) {
+  setTimeout(function() {
+    cb(x + y);
+  }, 1000);
+}
+
+var addThunk = makeThunk(addAsync, 4, 6); // thunked addAsync function
+
+addThunk();
+```
+
+**checkout ex2**
+
+### 2.2 More complex thunk application
+
+```js
+function eagerThunk(fn, ...args) {
+  var result, callback;
+
+  fn(...args, function(res) {
+    if (!callback) {
+      result = res;
+    } else {
+      return callback(res);
+    }
+  });
+
+  return function(cb) {
+    if (!result) {
+      callback = cb;
+    } else {
+      return cb(result);
+    }
+  };
+}
+```
+
+When we greate the thunk, the call of the passed in function will be immiditely executed.
+
+The best part of thunk is the time won't have impact on how we write the code.
--- exercises/ex2/ex2.js
+++ exercises/ex2/ex2.js
@@ -1,27 +1,112 @@
-function fakeAjax(url,cb) {
-	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
-	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+function fakeAjax(url, cb) {
+  var fake_responses = {
+    file1: "The first text",
+    file2: "The middle text",
+    file3: "The last text"
+  };
+  var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
 
-	console.log("Requesting: " + url);
+  console.log("Requesting: " + url);
 
-	setTimeout(function(){
-		cb(fake_responses[url]);
-	},randomDelay);
+  setTimeout(function() {
+    cb(fake_responses[url]);
+  }, randomDelay);
 }
 
 function output(text) {
-	console.log(text);
+  console.log(text);
 }
 
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+  // what do we do here?
+  var callbackPassedIn; // this variable will save the callback that passed in from the place where we make the thunk
+  var fileContent; // this variable will save the text that come back from the fakeAjax
+
+  fakeAjax(file, function(text) {
+    if (!callbackPassedIn) {
+      // by the time we got the response, we haven't call the thunk created previously
+      fileContent = text; // we save the value in the closure and call later when we call the thunk
+    } else {
+      // by the time we got the response, we already called the thunk, means we got a callbackPassedIn
+      callbackPassedIn(text);
+    }
+  });
+
+  return function thunked(cb) {
+    if (!fileContent) {
+      // by the time we call the thunk, we haven't got the response yet, we save the callback
+      callbackPassedIn = cb;
+    } else {
+      // by the time we call the thunk, we already got the response, we just call the cb with the previously saved response
+      cb(fileContent);
+    }
+  };
 }
+
+var th1 = getFile("file1");
+var th2 = getFile("file2");
+var th3 = getFile("file3");
+
+th1(function(text) {
+  output(text);
+  th2(function(text) {
+    output(text);
+    th3(function(text) {
+      output(text);
+      output("completed");
+    });
+  });
+});
+
+// setTimeout(function() {
+//   th1(function(text) {
+//     output(text);
+//     th2(function(text) {
+//       output(text);
+//       th3(function(text) {
+//         output(text);
+//       });
+//     });
+//   });
+// }, 10000);
 
 // request all files at once in "parallel"
 // ???
+
+// solution 2
+// function thunkify(fn, ...args) {
+//   var result, callback;
+
+//   fn(...args, function(res) {
+//     if (!callback) {
+//       result = res;
+//     } else {
+//       return callback(res);
+//     }
+//   });
+
+//   return function(cb) {
+//     if (!result) {
+//       callback = cb;
+//     } else {
+//       return cb(result);
+//     }
+//   };
+// }
+
+// var th1 = thunkify(fakeAjax, "file1");
+// var th2 = thunkify(fakeAjax, "file2");
+// var th3 = thunkify(fakeAjax, "file3");
+
+// th1(function(text) {
+//   output(text);
+//   th2(function(text) {
+//     output(text);
+//     th3(function(text) {
+//       output(text);
+//       output("completed");
+//     });
+//   });
+// });
